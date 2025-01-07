19b9a1643233bec6c3c1debcffc9219dd7830525
sapaza <saveliy.zaev@outlook.com>
2025-01-07 10:01:09 +0300

    Add files via upload

--- Программа_13.py
+++ Программа_13.py
@@ -2,15 +2,15 @@ import random
 
 t = ("бриллианты", "рубины", "изумруды", "золотые монеты", "жемчуг")
 
-chest1 = ""
-chest2 = ""
-chest3 = ""
-x = 2
+chest1 = ()
+chest2 = ()
+chest3 = ()
 
-chest1 = random.choice(t)
-chest2 = random.choice(t)
-chest3 = random.choice(t)
+for i in range(5):
+    chest1 = chest1 + (random.choice(t),)
+    chest2 = chest2 + (random.choice(t),)
+    chest3 = chest3 + (random.choice(t),)
 
-print(f"Сундук 1: '{random.choice(t)}', '{random.choice(t)}', '{random.choice(t)}', '{random.choice(t)}', '{random.choice(t)}'")
-print(f"Сундук 1: '{random.choice(t)}', '{random.choice(t)}', '{random.choice(t)}', '{random.choice(t)}', '{random.choice(t)}'")
-print(f"Сундук 1: '{random.choice(t)}', '{random.choice(t)}', '{random.choice(t)}', '{random.choice(t)}', '{random.choice(t)}'")
+print("Сундук 1:", chest1)
+print("Сундук 2:", chest2)
+print("Сундук 3:", chest3)
