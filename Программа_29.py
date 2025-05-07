c60aad93d6dc41e93b47c7a7b74e629bd37b940a
sapaza <saveliy.zaev@outlook.com>
2025-05-07 09:38:27 +0300

    Add files via upload

---
+++ Программа_29.py
@@ -0,0 +1,38 @@
+from turtle import *
+speed(0)
+
+def move_fd():
+    setheading(90)
+    fd(10)
+def rotate_lt():
+    setheading(180)
+    fd(10)
+def move_bd():
+    setheading(270)
+    fd(10)
+def rotate_rt():
+    setheading(0)
+    fd(10)
+def move_stop():
+    home()
+def red():
+    color("red")
+def green():
+    color("green")
+def blue():
+    color("blue")
+screen = Screen()
+
+screen.onkeypress(move_fd,"w")
+screen.onkeypress(rotate_lt,"a")
+screen.onkeypress(move_bd,"s")
+screen.onkeypress(rotate_rt,"d")
+screen.onkeypress(red,"r")
+screen.onkeypress(green,"g")
+screen.onkeypress(blue,"b")
+
+
+screen.onscreenclick(move_stop)
+
+screen.listen()
+screen.mainloop()
